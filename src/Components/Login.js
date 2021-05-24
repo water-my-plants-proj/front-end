b5b8992f95138f88f07a080e45eeea83a28fbefb
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-05-24 14:51:26 -0500

    Form structure and functions complete. Navigation folder added, LoginSchema added.

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,6 +1,16 @@
+// I did an npm i
+
+
 import React, { useState } from 'react'
+import * as yup from 'yup'
+import schema from './validation/LoginSchema'
 
 const initialLoginValues = {
+	username: '',
+	phoneNum: '',
+	password: ''
+}
+const initialLoginErrors = {
 	username: '',
 	phoneNum: '',
 	password: ''
@@ -9,13 +19,20 @@ const initialDisabled = true;
 
 const [loginValues, setLoginValues] = useState(initialLoginValues);
 const [disabled, setDisabled] = useState(initialDisabled);   //need to add disabled button functionality based on validation
+const [formErrors, setFormErrors] = useState(initialLoginErrors);
 
 export default function Login() {   //removed props, dont know what will be passed in
-	// const { loginValues, disabled, submit, change } = props;
+
+	const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: ''}))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0]}))
+  }
 
 	const onSubmit = evt => {
     evt.preventDefault()
-    submit() //not sure how this will need to be set up
+    //submit() //not sure how this will need to be set up
   }
 
 	//receiving token for auth
@@ -42,36 +59,36 @@ export default function Login() {   //removed props, dont know what will be pass
   //   login(newLogin)
   // }
 
-    return (
-        <div>
-            <form className='loginContainer' onSubmit={onSubmit}>
-							<h2>Login</h2>
-							<label>Username:
-								<input 
-									id='usernameInput' 
-									type='text' 
-									name='username' 
-									value={loginValues.username}
-									onChange={onChange}/>
-							</label>
-							<label>Phone Number:
-								<input 
-									id='phoneNumInput' 
-									type='text' 
-									name='phoneNum' 
-									value={loginValues.phoneNum}
-									onChange={onChange}/>
-							</label>
-							<label>Password:
-								<input 
-									id='passwordInput' 
-									type='password' 
-									name='password' 
-									value={loginValues.password}
-									onChange={onChange}/>
-							</label>
-							<button disabled={disabled}>Log In</button>
-						</form>
-        </div>
-    )
+	return (
+			<div>
+					<form className='loginContainer' onSubmit={onSubmit}>
+						<h2>Login</h2>
+						<label>Username:
+							<input 
+								id='usernameInput' 
+								type='text' 
+								name='username' 
+								value={loginValues.username}
+								onChange={onChange}/>
+						</label>
+						<label>Phone Number:
+							<input 
+								id='phoneNumInput' 
+								type='text' 
+								name='phoneNum' 
+								value={loginValues.phoneNum}
+								onChange={onChange}/>
+						</label>
+						<label>Password:
+							<input 
+								id='passwordInput' 
+								type='password' 
+								name='password' 
+								value={loginValues.password}
+								onChange={onChange}/>
+						</label>
+						<button disabled={disabled}>Log In</button>
+					</form>
+			</div>
+	)
 }
---
+++ src/validation/LoginSchema.js
@@ -0,0 +1,18 @@
+import * as yup from 'yup'
+
+const loginSchema = yup.object().shape({
+    username: yup
+        .string()
+        .trim()
+        .required('Username is required'),
+    phoneNum: yup
+        .string()
+        .trim()
+        .required('Phone number is required'),
+    password: yup
+        .string()
+				.trim()
+				.required('Password is required')
+})
+
+export default loginSchema
