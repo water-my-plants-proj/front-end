9271b0fbc4d7ab34e51f926e4852c2f89b5dc2d7
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-05-24 12:16:41 -0500

    Login Form Setup, WIP

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,47 @@
 import React from 'react'
 
-export default function Login() {
+export default function Login(props) {
+	const { loginValues, disabled, submit, change } = props;
+
+	const onSubmit = evt => {
+    evt.preventDefault()
+    submit()
+  }
+
+	const onChange = evt => {
+		const { name, value } = evt.target
+		change(name, value)
+	}
     return (
         <div>
-            
+            <form className='login container' onSubmit={onSubmit}>
+							<h2>Login</h2>
+							<label>Username:
+								<input 
+									id='usernameInput' 
+									type='text' 
+									name='username' 
+									value={loginValues.username}
+									onChange={onChange}/>
+							</label>
+							<label>Phone Number:
+								<input 
+									id='phoneNumInput' 
+									type='text' 
+									name='phoneNum' 
+									value={loginValues.phoneNum}
+									onChange={onChange}/>
+							</label>
+							<label>Password:
+								<input 
+									id='passwordInput' 
+									type='password' 
+									name='password' 
+									value={loginValues.password}
+									onChange={onChange}/>
+							</label>
+							<button disabled={disabled}>Log In</button>
+						</form>
         </div>
     )
 }
