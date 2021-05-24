f87bbc940af259bedbdfac826bb3f968f1748824
Daniel Walker <dw765608@gmail.com>
2021-05-24 13:43:18 -0400

    basic home/landingPage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,59 @@
 import React from 'react'
-
+import landingImg from '../Img/homeImg.gif'
+import {makeStyles} from '@material-ui/styles'
+const useStyles = makeStyles({
+container:{
+    margin:"0 auto",
+    display:"flex",
+    flexDirection:"column",
+    border:"10px solid green",
+    borderRadius:"20%",
+    width:"50%"
+},
+header:{
+margin:"0 auto",
+marginTop:"5%",
+fontSize:"2vw"
+},
+img:{
+backgroundImage:`url(${landingImg})`,
+padding:"30%",
+backgroundRepeat:"no-repeat",
+backgroundSize:"cover",
+width:"10%",
+height:"10%",
+borderRadius:"20%"
+},
+signUp:{
+width:"20%",
+margin:"0 auto",
+backgroundColor:"green",
+marginBottom:"5%",
+borderRadius:"10%",
+},
+logIn:{
+    width:"20%",
+    margin:"0 auto",
+    backgroundColor:"green",
+    marginBottom:"5%",
+    borderRadius:"10%",
+    }
+})
 export default function Home() {
+    const handleSignIn=(e)=>{
+        e.preventDefault();
+    }
+    const handleLogIn=(e)=>{
+        e.preventDefault();
+    }
+    const classes = useStyles();
+
     return (
-        <div>
-            
+        <div className={classes.container}>
+            <h1 className={classes.header}>WELCOME TO WATER MY PLANTS!!</h1>
+            <div className={classes.img} ></div>
+            <button onClick={handleSignIn} className={classes.signUp}>SIGN UP</button>
+            <button onClick={handleLogIn} className={classes.logIn}>lOG IN</button>
         </div>
     )
 }
