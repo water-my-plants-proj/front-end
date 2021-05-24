9b8b78b5b816729357b624042bc201b22e16ec19
devin mitchell <devin.a.mitchell@gmail.com>
2021-05-24 12:42:32 -0600

    building reducer

---
+++ src/Reducers/Index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const initialState = {
+    plantList: [
+            {}
+        ],
+    isFetching: false,
+    error: '', 
+
+}
+
+export const Reducer = (state=initialState, action) => {
+    switch(action.type) {
+        case A:
+            return state
+        default:
+            return state
+    }
+}
