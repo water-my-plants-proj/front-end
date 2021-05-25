65cba156fe8e387e76970125369556357055a4e1
Daniel Walker <dw765608@gmail.com>
2021-05-25 14:55:37 -0400

    DummyData Added and sign in and log in go to plantList

--- src/Components/PlantList.js
+++ src/Components/PlantList.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import DummyData from '../DummyData'
 
 export default function PlantList() {
     return (
         <div>
-            
+            {DummyData.map((item)=>{
+                return (
+                    <div>
+                       <p>{item.species}</p> 
+                        <p>{item.nickname}</p>
+                        <p>{item.h20Frequency}</p>
+                        <img src={item.image}></img>
+                    </div>
+                )
+            })}
         </div>
     )
 }
