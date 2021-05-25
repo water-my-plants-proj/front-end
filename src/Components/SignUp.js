111620ce42ff128140a66f64c355b32bf270c920
Daniel Walker <dw765608@gmail.com>
2021-05-24 21:11:51 -0400

    Added Axios With Auth and changed base in utils

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import axios from 'axios';
+import React,{useState} from 'react'
+import axiosWithAuth from '../Utils/AxiosWithAuth'
+const initialValue={
+    username: "Joe",
+    password: "1234",
+    phoneNumber:"775-123-1456"
+}
 
 export default function SignUp() {
-    return (
-        <div>
-            
-        </div>
-    )
+
+	const [signUpValues, setSignUp] = useState(initialValue);
+
+	const onSubmit = e => {
+		e.preventDefault()
+        axiosWithAuth().post("auth/register",signUpValues)
+        .then((res) => {
+            console.log(res)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+	}
+
+	const onChange = e => {
+		const { name, value } = e.target;
+        setSignUp({
+			...signUpValues,
+			[name]: value
+		})
+	}
+    console.log(signUpValues.phoneNumber)
+	return (
+
+		<div>
+			<form className='loginContainer' onSubmit={onSubmit}>
+				<h2>Sign Up</h2>
+				<label>Username:
+					<input
+						type='text'
+						name='username'
+						value={signUpValues.username}
+						onChange={onChange} />
+				</label>
+				<label>Phone Number:
+					<input
+						type='text'
+						name='phoneNumber'
+						value={signUpValues.phoneNumber}
+						onChange={onChange} />
+				</label>
+				<label>Password:
+							<input
+						type='password'
+						name='password'
+						value={signUpValues.password}
+						onChange={onChange} />
+				</label>
+				<button>Sign Up</button>
+			</form>
+		</div>
+	)
 }
