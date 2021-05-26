ff16ac46dbde6f1a6ee17f684ed51f977028fac6
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-05-26 11:55:53 -0500

    Forgot to add changes

--- src/Components/EditUser.js
+++ src/Components/EditUser.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
-export default function EditUser() {
-    return (
-        <div>
-            
-        </div>
-    )
+const styledEditUserPage = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+`;
+
+export default function EditUser(props) {
+  const { user } = props;
+
+  return (
+    <styledEditUserPage>
+      <div>
+        <h2>Username: {user.username}</h2>
+        <h2>Password: {user.password}</h2>
+      </div>
+      <div>
+        <form>
+          <label>
+            Username:
+            <input />
+          </label>
+          <label>
+            Password:
+            <input />
+          </label>
+        </form>
+      </div>
+    </styledEditUserPage>
+  );
 }
