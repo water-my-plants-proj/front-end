7118055ccdb755ea657a473b30f360dca5749b89
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-05-26 14:50:14 -0500

    Added validation file

---
+++ src/validation/EditUserSchema.js
@@ -0,0 +1,8 @@
+import * as yup from "yup";
+
+const editUserSchema = yup.object().shape({
+  username: yup.string().trim().required("Username is required"),
+  password: yup.string().trim().required("Password is required"),
+});
+
+export default editUserSchema;
