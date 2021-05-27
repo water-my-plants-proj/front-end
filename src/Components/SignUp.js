cb9a6809f514963014b4e95064bc247a8a10ede2
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-05-27 14:00:28 -0500

    Formatted SignUp

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
-import React,{useState} from 'react'
-import {useHistory} from 'react-router-dom'
-import styled from 'styled-components'
+import axios from "axios";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
 
 const StyledDiv = styled.div`
 	margin: 0 auto;
@@ -38,7 +38,7 @@ const StyledDiv = styled.div`
   }
   .buttonContainer {
 		display:flex;
-		justify-content: center;
+		justify-content: center
     margin-top: 2%;
   }
   .errors {
@@ -46,66 +46,72 @@ const StyledDiv = styled.div`
     color: red;
     font-size: 0.5rem;
   }
-`
+`;
 
-const initialValue={
-    username: "Test",
-    password: "Password",
-    phoneNumber:"123-123-1234"
-}
+const initialValue = {
+  username: "Test",
+  password: "Password",
+  phoneNumber: "123-123-1234",
+};
 
 export default function SignUp() {
-	const {push}=useHistory()
-	const [signUpValues, setSignUp] = useState(initialValue);
-	const onSubmit = e => {
-		 e.preventDefault()
-    	 axios.post("https://plantszapi.herokuapp.com/api/auth/register",signUpValues)
-        .then((res) => {
-            console.log(res)
-        })
-        .catch((err) => {
-            console.log(err)
-         })
-		push("/plant-list")
-	}
+  const { push } = useHistory();
+  const [signUpValues, setSignUp] = useState(initialValue);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://plantszapi.herokuapp.com/api/auth/register", signUpValues)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    push("/plant-list");
+  };
 
-	const onChange = e => {
-		const { name, value } = e.target;
-        setSignUp({
-			...signUpValues,
-			[name]: value
-		})
-	}
-	return (
-
-		<StyledDiv>
-			<form className='loginContainer' onSubmit={onSubmit}>
-				<h2>Sign Up</h2>
-				<label>Username:
-					<input
-						type='text'
-						name='username'
-						value={signUpValues.username}
-						onChange={onChange} />
-				</label>
-				<label>Phone Number:
-					<input
-						type='text'
-						name='phoneNumber'
-						value={signUpValues.phoneNumber}
-						onChange={onChange} />
-				</label>
-				<label>Password:
-							<input
-						type='password'
-						name='password'
-						value={signUpValues.password}
-						onChange={onChange} />
-				</label>
-				<div className='buttonContainer'>
-					<button>Sign Up</button>
-				</div>
-			</form>
-		</StyledDiv>
-	)
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setSignUp({
+      ...signUpValues,
+      [name]: value,
+    });
+  };
+  return (
+    <StyledDiv>
+      <form className="loginContainer" onSubmit={onSubmit}>
+        <h2>Sign Up</h2>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={signUpValues.username}
+            onChange={onChange}
+          />
+        </label>
+        <label>
+          Phone Number:
+          <input
+            type="text"
+            name="phoneNumber"
+            value={signUpValues.phoneNumber}
+            onChange={onChange}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={signUpValues.password}
+            onChange={onChange}
+          />
+        </label>
+        <div className="buttonContainer">
+          <button>Sign Up</button>
+        </div>
+      </form>
+    </StyledDiv>
+  );
 }
