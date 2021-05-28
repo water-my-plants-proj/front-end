655f5dcf1044ef3a2f0e90aa74f76a201bb1ff19
devin mitchell <devin.a.mitchell@gmail.com>
2021-05-28 13:03:09 -0600

    fixed add plant styles

--- src/Components/AddPlant.js
+++ src/Components/AddPlant.js
@@ -7,56 +7,62 @@ import { connect } from "react-redux";
 import { addPlant } from '../Actions/Index';
 import { useHistory } from 'react-router-dom';
 
-const StyledFormAddPlant = styled.div`
+const Form = styled.form`
+  position: relative;
+  top: 150px;
+  height: 50vh;
+  width: 30%;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  border: 2px solid #52734d;
-  background-color: #ddffbc;
+  color: #f3f2da;
+  border: 1px solid #f3f2da; 
+  box-shadow: 0px 0px 60px #ea97ad;
+  background-color: black;
   font-family: "Lato";
-  width: 30%;
-  margin-top: 15%;
 
-  h1,
-  h2,
-  h3,
-  h4 {
-    font-family: "Roboto";
-  }
-  .newPlantContainer {
-    display: flex;
-    flex-direction: column;
-  }
   label {
     display: flex;
     justify-content: space-between;
+    width: 
   }
 
   h2 {
-    margin: 0 auto;
-    margin-bottom: 2%;
+  font-family: "Roboto";
+  margin: 25px auto;
+  margin-top: 5%;
+  font-size: 2rem;
+  text-align: center;
+  border-bottom: 3px solid #f3f2da;
   }
+
   button {
-    width: 40%;
-    color: #feffde;
-    background-color: #91c788;
+    background: #f3f2da;
+    margin: 0 10px;
+    border-radius: 10px;
+    padding: 2px 25px;
+    font-size: 1rem;
+    outline: none;
+
+    &:hover {
+      background: black;
+      color: #f3f2da;
+      border: 2px solid #f3f2da;
   }
-  input {
-    background-color: #feffde;
+  &:active {
+      transform: scale(.9);
+      box-shadow: inset 2px 2px 5px white;
   }
-  .buttonContainer {
-    display: flex;
-    justify-content: center;
-    margin-top: 2%;
   }
+ 
   .errors {
     display: flex;
     color: red;
     font-size: 0.5rem;
   }
-`;
+`
 
 const initialPlantValues = {
   nickname: "",
@@ -128,49 +134,47 @@ function AddPlant(props) {
   }, [plantValues]);
 
   return (
-    <StyledFormAddPlant>
-      <form className="newPlantContainer" onSubmit={onSubmit}>
-        <h2>Add a New Plant</h2>
-        <label>
-          Nickname:
-          <input
-            id="nicknameInput"
-            type="text"
-            name="nickname"
-            value={plantValues.nickname}
-            onChange={onChange}
-          />
-        </label>
-        <label>
-          Plant Species:
-          <input
-            id="speciesInput"
-            type="text"
-            name="species"
-            value={plantValues.phoneNum}
-            onChange={onChange}
-          />
-        </label>
-        <label>
-          Watering Frequency:
-          <input
-            id="h2oFrequencyInput"
-            type="text"
-            name="h2oFrequency"
-            value={plantValues.password}
-            onChange={onChange}
-          />
-        </label>
-        <div className="buttonContainer">
-          <button disabled={disabled}>Add Plant</button>
-        </div>
-        <div className="errors">
-          <h3>{plantErrors.nickname}</h3>
-          <h3>{plantErrors.species}</h3>
-          <h3>{plantErrors.h2oFrequency}</h3>
-        </div>
-      </form>
-    </StyledFormAddPlant>
+        <Form className="newPlantContainer" onSubmit={onSubmit}>
+          <h2>Add a New Plant</h2>
+          <label>
+            Nickname:
+            <input
+              id="nicknameInput"
+              type="text"
+              name="nickname"
+              value={plantValues.nickname}
+              onChange={onChange}
+            />
+          </label>
+          <label>
+            Plant Species:
+            <input
+              id="speciesInput"
+              type="text"
+              name="species"
+              value={plantValues.phoneNum}
+              onChange={onChange}
+            />
+          </label>
+          <label>
+            Watering Frequency:
+            <input
+              id="h2oFrequencyInput"
+              type="text"
+              name="h2oFrequency"
+              value={plantValues.password}
+              onChange={onChange}
+            />
+          </label>
+          <div className="buttonContainer">
+            <button disabled={disabled}>Add Plant</button>
+          </div>
+          <div className="errors">
+            <h3>{plantErrors.nickname}</h3>
+            <h3>{plantErrors.species}</h3>
+            <h3>{plantErrors.h2oFrequency}</h3>
+          </div>
+      </Form>
   );
 }
 const mapStateToProps = (state) => {
