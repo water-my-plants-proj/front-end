afc149bdabd47fb7236ed0412848c27fa888f3c2
devin mitchell <devin.a.mitchell@gmail.com>
2021-05-28 12:50:12 -0600

    finished styling signup and login forms

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -5,6 +5,65 @@ import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 
+
+
+const Form = styled.form`
+  position: relative;
+  top: 150px;
+  height: 50vh;
+  width: 30%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  color: #f3f2da;
+  border: 1px solid #f3f2da; 
+  box-shadow: 0px 0px 60px #ea97ad;
+  background-color: black;
+  font-family: "Lato";
+
+  label {
+    display: flex;
+    justify-content: space-between;
+    width: 
+  }
+
+  h2 {
+  font-family: "Roboto";
+  margin: 25px auto;
+  margin-top: 5%;
+  font-size: 2rem;
+  text-align: center;
+  border-bottom: 3px solid #f3f2da;
+  }
+
+  button {
+    background: #f3f2da;
+    margin: 0 10px;
+    border-radius: 10px;
+    padding: 2px 25px;
+    font-size: 1rem;
+    outline: none;
+
+    &:hover {
+      background: black;
+      color: #f3f2da;
+      border: 2px solid #f3f2da;
+    }
+    &:active {
+        transform: scale(.9);
+        box-shadow: inset 2px 2px 5px white;
+    }
+  }
+ 
+  .errors {
+    display: flex;
+    color: red;
+    font-size: 0.5rem;
+  }
+`
+
 const initialLoginValues = {
   username: "test",
   password: "1234",
@@ -16,58 +75,6 @@ const initialLoginErrors = {
 };
 
 const initialDisabled = true;
-
-const StyledFormLogin = styled.div`
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border: 2px solid #52734d;
-  background-color: #ddffbc;
-  font-family: "Lato";
-  width: 30%;
-
-
-  h1,
-  h2,
-  h3,
-  h4 {
-    font-family: "Roboto";
-  }
-
-  .loginContainer {
-    display: flex;
-    flex-direction: column;
-  }
-  label {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  h2 {
-    margin: 0 auto;
-    margin-bottom: 2%;
-  }
-  button {
-    width: 40%;
-    color: #feffde;
-    background-color: #91c788;
-  }
-  input {
-    background-color: #feffde;
-  }
-  .buttonContainer {
-    display: flex;
-    justify-content: center;
-    margin-top: 2%;
-  }
-  .errors {
-    display: flex;
-    color: red;
-    font-size: 0.5rem;
-  }
-`;
 
 export default function Login(props) {
   const test = {
@@ -122,38 +129,36 @@ export default function Login(props) {
   }, [loginValues]);
 
   return (
-    <StyledFormLogin>
-      <form className="loginContainer" onSubmit={onSubmit}>
-        <h2>Login</h2>
-        <label>
-          Username:
-          <input
-            id="usernameInput"
-            type="text"
-            name="username"
-            value={loginValues.username}
-            onChange={onChange}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            id="passwordInput"
-            type="password"
-            name="password"
-            value={loginValues.password}
-            onChange={onChange}
-          />
-        </label>
-        <div className="buttonContainer">
-          <button disabled={disabled}>Log In</button>
-        </div>
+    <Form className="loginContainer" onSubmit={onSubmit}>
+      <h2>Login</h2>
+      <label>
+        Username:
+        <input
+          id="usernameInput"
+          type="text"
+          name="username"
+          value={loginValues.username}
+          onChange={onChange}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          id="passwordInput"
+          type="password"
+          name="password"
+          value={loginValues.password}
+          onChange={onChange}
+        />
+      </label>
+      <div className="buttonContainer">
+        <button disabled={disabled}>Log In</button>
+      </div>
 
-        <div className="errors">
-          <h3>{formErrors.username}</h3>
-          <h3>{formErrors.password}</h3>
-        </div>
-      </form>
-    </StyledFormLogin>
+      <div className="errors">
+        <h3>{formErrors.username}</h3>
+        <h3>{formErrors.password}</h3>
+      </div>
+    </Form>
   );
 }
